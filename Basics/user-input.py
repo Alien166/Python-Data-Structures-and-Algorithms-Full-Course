a2fa395dd558469c092c418074b8ed3ef5c1a22d
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-25 20:01:56 +0300

    Create user-input.py

---
+++ Basics/user-input.py
@@ -0,0 +1,8 @@
+# User Input
+name = input("what's Your Name") # str by default
+age = int(input("what's Your age")) # convert to int
+height = float(input("what's Your height"))# convert to float
+
+print("Hello " +name)
+print("You are " + str(age) +" Years old") # must be covert to string
+print(height)
