e2cdd7e53e8f446b20f7197368b0e9b9961b22ad
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-25 17:38:49 +0300

    Create variables.py

---
+++ Basics/variables.py
@@ -0,0 +1,21 @@
+name= False
+print(type(name)) # to know the kind of data type
+
+# string
+first_name="abdeltawab"
+last_name="Wagieh"
+full_name=first_name+last_name
+print("Hello "+full_name)
+
+# int
+age=20;
+age +=1
+print(age)
+print("Your Age Is " + str(age))
+
+# Float
+my_age=20.0
+my_age +=21
+print(my_age)
+print(type(my_age))  # Float
+
