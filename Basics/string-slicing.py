85a1205461d8eceb659d185b765855c4c2c115c0
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-26 00:24:32 +0300

    Create string-slicing.py

---
+++ Basics/string-slicing.py
@@ -0,0 +1,11 @@
+
+name = "Bro Code"
+
+first_name = name[0:3]  # or [:3]
+print(first_name)
+last_name = name[4:] # from 4 to end
+print(last_name)
+funky_name = name[0:8:1] # three term for steps
+print(funky_name)
+reversed_name = name[::-1]
+print(reversed_name)
