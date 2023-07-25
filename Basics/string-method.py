b80a6ffb60acf8a50ab0c66ec9cb360d62d8566e
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-25 17:59:27 +0300

    Create string-method.py

---
+++ Basics/string-method.py
@@ -0,0 +1,16 @@
+name = "Bro"
+
+
+print(len(name))
+print(name.find("o"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("o"))
+print(name.replace("o", "r"))
+print(name*3)
+
+
+
