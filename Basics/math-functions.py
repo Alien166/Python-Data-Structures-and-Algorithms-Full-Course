fcd1852b0292806993551c5f7ff8f4e14187f808
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-25 23:03:10 +0300

    Create math-functions.py

---
+++ Basics/math-functions.py
@@ -0,0 +1,16 @@
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+# math functions
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(math.sqrt(233))
+print(abs(pi))
+print(pow(pi, 2))
+print(max(x, y, z))
