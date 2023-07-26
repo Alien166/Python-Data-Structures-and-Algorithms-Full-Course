537b611454847560fd4d44a3f9836afdf10a5dac
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-26 14:25:49 +0300

    Create if-statement.py

---
+++ Basics/if-statement.py
@@ -0,0 +1,13 @@
+
+age = int(input("how old are u"))
+
+if age == 100:
+    print("you are  a century old")
+elif age >= 12:
+    print("you are child")
+
+elif age <= 50:
+    print("you are adult")
+
+else:
+    print("you haven't been born")
