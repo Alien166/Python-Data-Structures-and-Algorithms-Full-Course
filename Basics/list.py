8283337a06bafeb82ffc17196519aca6e7ad57c2
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-08-02 16:13:53 +0300

    Create list.py

---
+++ Basics/list.py
@@ -0,0 +1,9 @@
+food= ["Pizza","coke","susi","blue","red"]
+food [0] ="flag"
+print(food)
+food.append("ice cream") #in last last
+food.insert(2,"Rote")
+food.remove("blue")
+food.sort()
+food.pop() # poping last element
+food.clear() # remove all element of the list
