ea114b92605174b53d45677c180842a93a510c55
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-08-02 15:29:11 +0300

    Create for-loop.py

---
+++ Basics/for-loop.py
@@ -0,0 +1,13 @@
+import time
+
+for i in range(10):
+    print(i)
+
+for i in range(50,100):
+    print(i)
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+
+print("Happy New Year !")
