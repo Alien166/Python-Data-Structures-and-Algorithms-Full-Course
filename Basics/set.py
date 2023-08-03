d31ce23ac7772bb2a6c3d6b1c36829fbaa2b6407
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-08-03 15:25:34 +0300

    Create set.py

---
+++ Basics/set.py
@@ -0,0 +1,10 @@
+uteniles = {"forks","spoon"}
+
+dishes = {"bowl","plate","cup"}
+
+uteniles.add("hapkin")
+uteniles.remove("forks")
+print(uteniles)
+
+dinner_table = uteniles.union(dishes)
+print(dinner_table)
